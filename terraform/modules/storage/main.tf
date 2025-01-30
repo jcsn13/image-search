c3d22fcc98a6e5294e9e0fc69d664b224b4682a5
jcsn13 <joseclaudiosoaresnetO@gmail.com>
2025-01-29 21:38:28 -0300

    Fix firestore

--- terraform/modules/storage/main.tf
+++ terraform/modules/storage/main.tf
@@ -1,3 +1,20 @@
+# Enable Firestore API
+resource "google_project_service" "firestore" {
+  service = "firestore.googleapis.com"
+  disable_on_destroy = false
+}
+
+# Create Firestore Database
+resource "google_firestore_database" "default" {
+  project     = var.project_id
+  name        = "(default)"
+  location_id = var.region
+  type        = "FIRESTORE_NATIVE"
+
+  depends_on = [google_project_service.firestore]
+}
+
+# Storage Buckets
 resource "google_storage_bucket" "raw_images" {
   name     = "${var.project_id}-raw-images"
   project  = var.project_id
